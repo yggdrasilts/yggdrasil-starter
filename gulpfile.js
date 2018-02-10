34c105888d4ddb9936def7c9db0f08d09c134a4d
telco2011 <davidlopez.david@gmail.com>
2018-02-10 16:52:22 +1000

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,1 @@
-var fs = require('fs');
-
-const gulpClass = './node_modules/@yggdrasil/devs/class.gulp.js';
-
-try {
-  if (fs.statSync(gulpClass)) {
-    console.log('Using @yggdrasil gulpclass.');
-    eval(fs.readFileSync(gulpClass).toString());
-  }
-} catch (error) {
-  console.log(error);
-}
+require('./node_modules/@yggdrasil/devs/yggdrasil.gulp.js');
